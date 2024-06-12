63038ed8817d0f3b68d138930a13867287e27aa5
filippo-viti <filippo.viti2002@gmail.com>
2024-06-12 15:51:39 +0200

    Removed unused files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import HeaderNavbar from "./components/header/HeaderNavbar.tsx";
 import {Container} from "react-bootstrap";
 import {LocationTabs} from "./components/forecasts/LocationTabs.tsx";
