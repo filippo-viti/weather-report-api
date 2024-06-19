520287dee85cd69849c0d0f460ea17a9bf3e543d
filippo-viti <filippo.viti2002@gmail.com>
2024-06-19 19:01:12 +0200

    Changed position of AuthProvider wrapping

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,16 @@
 import HeaderNavbar from "./components/header/HeaderNavbar.tsx";
 import {Container} from "react-bootstrap";
 import {LocationTabs} from "./components/forecasts/LocationTabs.tsx";
-import {AuthProvider} from "./components/authentication/AuthProvider.tsx";
 
 function App() {
 
   return (
-    <AuthProvider>
+    <>
       <HeaderNavbar/>
       <Container>
         <LocationTabs/>
       </Container>
-    </AuthProvider>
+    </>
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import {AuthProvider} from "./components/authentication/AuthProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App/>
+    </AuthProvider>
   </React.StrictMode>,
 )
