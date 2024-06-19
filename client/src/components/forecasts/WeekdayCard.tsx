0231f88f13fcf4d473afd83c5047e5836f17891a
filippo-viti <filippo.viti2002@gmail.com>
2024-06-19 18:22:45 +0200

    Limited temperature display to two decimal places

--- client/src/components/forecasts/WeekdayCard.tsx
+++ client/src/components/forecasts/WeekdayCard.tsx
@@ -1,23 +1,25 @@
-import {Card, CardBody, CardHeader, CardText, CardTitle, Row} from "react-bootstrap";
-import {WeatherDescription, WeatherForecast} from "../../types";
+import { Card, CardBody, CardHeader, CardText, CardTitle, Row } from "react-bootstrap";
+import { WeatherDescription, WeatherForecast } from "../../types";
 
-export default function WeekdayCard({weather}: { weather: WeatherForecast }) {
+export default function WeekdayCard({ weather }: { weather: WeatherForecast }) {
+  const formattedTemperature = weather.temperature.toFixed(2); // Limit to two decimal places
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>{weather.date}</CardTitle>
       </CardHeader>
       <CardBody>
-          <Row className={"text-center align-content-center fs-1"}>
-            <i className={getWeatherIcon(weather.description)}></i>
-          </Row>
-          <Row className={"text-center"}>
-            <CardText className={"m-0"}>{weather.description}</CardText>
-            <CardText>{weather.temperature}°C</CardText>
-          </Row>
+        <Row className={"text-center align-content-center fs-1"}>
+          <i className={getWeatherIcon(weather.description)}></i>
+        </Row>
+        <Row className={"text-center"}>
+          <CardText className={"m-0"}>{weather.description}</CardText>
+          <CardText>{formattedTemperature}°C</CardText>
+        </Row>
       </CardBody>
     </Card>
-  )
+  );
 }
 
 function getWeatherIcon(description: WeatherDescription) {
