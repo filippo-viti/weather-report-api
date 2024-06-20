009545dd26cdbfcfeb9640ee9c05245370d08d98
filippo-viti <filippo.viti2002@gmail.com>
2024-06-20 12:11:00 +0200

    Added conditional rendering for LocationTabs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
 import HeaderNavbar from "./components/header/HeaderNavbar.tsx";
 import {Container} from "react-bootstrap";
 import {LocationTabs} from "./components/forecasts/LocationTabs.tsx";
+import {useAuth} from "./components/authentication/AuthProvider.tsx";
 
 function App() {
+  const { accessToken } = useAuth();
 
   return (
     <>
       <HeaderNavbar/>
       <Container>
-        <LocationTabs/>
+        {accessToken ? (<LocationTabs/>):(<h1>Please login or sign up to see the weather forecast</h1>)}
       </Container>
     </>
   )
