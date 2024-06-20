8086fb5288eaa901c138dea30bf2c90ad4b0ea37
filippo-viti <filippo.viti2002@gmail.com>
2024-06-20 16:44:08 +0200

    Removed unused features

--- client/src/components/header/HeaderNavbar.tsx
+++ client/src/components/header/HeaderNavbar.tsx
@@ -1,4 +1,4 @@
-import {Alert, Col, Container, Form, Navbar, Row, Stack} from "react-bootstrap";
+import {Alert, Container, Navbar, Stack} from "react-bootstrap";
 import {ColorModeButton} from "./ColorModeButton.tsx";
 import ProfileButton from "./ProfileButton.tsx";
 import GitHubButton from "./GitHubButton.tsx";
@@ -42,19 +42,6 @@ export default function HeaderNavbar() {
       <Navbar expand="lg" bg={"primary"}>
         <Container>
           <Navbar.Brand href="#home">Weather Report</Navbar.Brand>
-          <Form>
-            <Row>
-              <Col xs="auto">
-                <Form.Control
-                  type="text"
-                  placeholder="Search location"
-                  className="mr-sm-2"
-                />
-              </Col>
-              <Col xs="auto">
-              </Col>
-            </Row>
-          </Form>
           <Stack className={"ms-auto"} direction={"horizontal"}>
             {accessToken && <ProfileButton/>}
             {!accessToken && <LoginButton/>}
